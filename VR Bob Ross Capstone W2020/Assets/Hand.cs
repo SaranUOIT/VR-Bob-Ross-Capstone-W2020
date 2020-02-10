58c2c86df52a50bd7b23e23b431f1f275929ef61
Saran <saran.krishnaraja@uoit.net>
2020-02-10 14:36:34 -0500

    For Hugo
    Added a base VR scene with Houges code. Needs to be tested with Oculus.

---
+++ VR Bob Ross Capstone W2020/Assets/Hand.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class Hand : MonoBehaviour
+{
+	// Unity Settings
+	public enum HandCode { Left, Right };
+	public HandCode HandType = HandCode.Left;
+	public bool UnparentFromVRController = false;
+
+	// VR Controls
+	bool isTriggerHeld = false;
+
+	// VR Settings
+	XRNode HandNode;
+
+    public bool Mouse1;
+    public bool Mouse2;
+    public bool Mouse3;
+
+    void Start()
+	{
+		if (HandType == HandCode.Left)
+		{
+			HandNode = XRNode.LeftHand;
+		}
+		else
+		{
+			HandNode = XRNode.RightHand;
+		}
+	}
+
+	void Update()
+	{
+		if (!UnparentFromVRController)
+		{
+			return;
+		}
+
+		TriggerUpdate();
+		TransformUpdate();
+	}
+
+	void TriggerUpdate()
+	{
+		isTriggerHeld = Input.GetAxisRaw(HandType == HandCode.Left
+			? "Left Trigger" : "Right Trigger") > 0.3f;
+
+		if (isTriggerHeld)
+		{
+            //...
+            Mouse1 = true;
+		}
+		else
+		{
+            //...
+            Mouse1 = false;
+        }
+    }
+
+	void TransformUpdate()
+	{
+		var states = new List<XRNodeState>();
+		InputTracking.GetNodeStates(states);
+
+		foreach (var state in states)
+		{
+			if (state.nodeType == HandNode)
+			{
+				Vector3 pos;
+				if (state.TryGetPosition(out pos))
+					transform.position = pos;
+
+				Quaternion rot;
+				if (state.TryGetRotation(out rot))
+					transform.rotation = rot;
+
+				return;
+			}
+		}
+	}
+}
---
+++ VR Bob Ross Capstone W2020/Assets/OculusInput.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class OculusInput : MonoBehaviour
+{
+    bool Mouse1;
+    bool Mouse2;
+    bool Mouse3;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool triggerDown = OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger);
+        bool triggerUp = OVRInput.GetUp(OVRInput.Button.PrimaryIndexTrigger);
+
+        if(triggerDown)
+        {
+            Mouse1 = true;
+        }
+        if(triggerUp)
+        {
+            Mouse1 = false;
+        }
+
+        if (OVRInput.GetDown(OVRInput.Button.One, OVRInput.Controller.RTouch))
+        {
+            Mouse2 = true;
+        }
+        if (OVRInput.GetUp(OVRInput.Button.One, OVRInput.Controller.RTouch))
+        {
+            Mouse2 = false;
+        }
+        if (OVRInput.GetDown(OVRInput.Button.Two, OVRInput.Controller.RTouch))
+        {
+            Mouse3 = true;
+        }
+        if (OVRInput.GetUp(OVRInput.Button.Two, OVRInput.Controller.RTouch))
+        {
+            Mouse3 = false;
+        }
+    }
+}
