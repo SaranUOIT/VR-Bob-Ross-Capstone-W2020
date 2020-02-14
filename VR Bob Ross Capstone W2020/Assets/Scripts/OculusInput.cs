75f570033d8c2ef661f559b2e79596ff614f3a9a
Saran <saran.krishnaraja@uoit.net>
2020-02-14 02:01:46 -0500

    Milestone 1
    Fixed canvas bug and added to scene. Added toon shading. Added post proccessing but did not enable it. Removed SteamVR. Tided up asset folder. Exported materials of some characters.

---
+++ VR Bob Ross Capstone W2020/Assets/Scripts/OculusInput.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OculusInput : MonoBehaviour
+{
+    bool Mouse1;
+    bool Mouse2;
+    bool Mouse3;
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool triggerDown = OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger);
+        bool triggerUp = OVRInput.GetUp(OVRInput.Button.PrimaryIndexTrigger);
+
+        if(triggerDown)
+        {
+            Mouse1 = true;
+        }
+        if(triggerUp)
+        {
+            Mouse1 = false;
+        }
+
+        if (OVRInput.GetDown(OVRInput.Button.One, OVRInput.Controller.RTouch))
+        {
+            Mouse2 = true;
+        }
+        if (OVRInput.GetUp(OVRInput.Button.One, OVRInput.Controller.RTouch))
+        {
+            Mouse2 = false;
+        }
+        if (OVRInput.GetDown(OVRInput.Button.Two, OVRInput.Controller.RTouch))
+        {
+            Mouse3 = true;
+        }
+        if (OVRInput.GetUp(OVRInput.Button.Two, OVRInput.Controller.RTouch))
+        {
+            Mouse3 = false;
+        }
+    }
+
+    public Ray CreateRay()
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        return ray;
+    }
+
+    public bool GetMouse1()
+    {
+        return Mouse1;
+    }
+    public bool GetMouse2()
+    {
+        return Mouse2;
+    }
+    public bool GetMouse3()
+    {
+        return Mouse3;
+    }
+}
