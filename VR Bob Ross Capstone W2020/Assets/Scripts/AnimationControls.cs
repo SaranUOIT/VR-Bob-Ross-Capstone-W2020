65bc0240af29ae8af639418faa35bad64ffebef8
Saran <saran.krishnaraja@uoit.net>
2020-03-20 06:19:06 -0400

    Animation Test
    Added and animation controller to pause, play and rewind animation. Needs to be tested in VR.

---
+++ VR Bob Ross Capstone W2020/Assets/Scripts/AnimationControls.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationControls : MonoBehaviour
+{
+    Animator anim;
+    float speed = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(speed);
+
+        anim.SetFloat("Speed", speed);
+    }
+
+    public void Play()
+    {
+        speed = 1;
+    }
+    public void Rewind()
+    {
+        speed = -1;
+    }
+    public void Pause()
+    {
+        speed = 0;
+    }
+}
